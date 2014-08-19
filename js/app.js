d4310681f9c160c059aec8c55a3e8f13d70e4c24
Seth Wieder <swieder227@gmail.com>
2014-08-19 00:28:38 -0400

    designing in browser. nav bar and basic #beauty

---
+++ js/app.js
@@ -0,0 +1,9 @@
+$( document ).ready(function() {
+  $(".menuBtn").on("click",function(){
+  	$("#rwd-menu").toggleClass("expanded");
+  });
+
+  window.setTimeout(function(){
+  	$(".warning").addClass("show");
+  },1000);
+});
