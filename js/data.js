c9d20397dc2ef0f06ebb2fe9923b8d8027df50ae
Seth Wieder <swieder227@gmail.com>
2014-12-01 10:12:53 -0500

    change up portfolio wordage. make homepage more legible

--- js/data.js
+++ js/data.js
@@ -2,22 +2,22 @@ var portfolioData = {
 	portfolioCards: [
 		{
 			name:"mi",
-			title:"Mobile App Design",
-			body:"I was hired by an entrepeneur take a prototype and design the user interface. To ensure a positive UX, I created wireframes and performed usability tests, before creating finalized UI assets.",
+			title:"Mobile App UX Design",
+			body:"I was hired by an entrepeneur to take a software prototype and produce a fully designed and tested user interface.",
 			tags:["ux","psd","ai"],
 			hero:"../img/portfolio/hero_mi.jpg",
 		},
 		{
 			name:"nimble",
-			title:"Interactive Web Apps",
-			body:"I am a front-end programmer. I use semantic HTML5, CSS3 animations, SASS, SVGs, Video/Audio, Gestures and Javascript/jQuery to make it all come to life.",
+			title:"Front-End Development",
+			body:"I code web sites and apps using HTML5, CSS3, and JS. Depending on the project, I've used CSS Preprocessors, HTML Video/Audio, and JS MVCs.",
 			tags:["html","css","js"],
 			hero:"../img/portfolio/hero_nimble.jpg",
 		},
 		{
 			name:"blues",
-			title:"Redesign & Custom Wordpress",
-			body:"I consulted for a small business by rebranding their website and social media presence. I implemented a Wordpress CMS and custom theme using underscores.me.",
+			title:"Branding & Tech Consulting",
+			body:"I consulted for a small business by rebranding their online presence. I redesigned their website, implemented a CMS, and expanded their brand across pertinent social media.",
 			tags:["psd","wp","webcode"],
 			hero:"../img/portfolio/hero_blues.jpg",
 		},
