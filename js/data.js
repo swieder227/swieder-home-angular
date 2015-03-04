1537bb753b38c8a90285ee4f2ef5f6f090ada9b9
Seth Wieder <swieder227@gmail.com>
2015-03-04 00:35:56 -0500

    WIP portfolio cards improvements. added title and cta to card hero

--- js/data.js
+++ js/data.js
@@ -90,22 +90,25 @@ var skillsData = {
 var portfolioData = {
 	portfolioCards: [
 		{
-			name:"mi",
-			title:"Mobile App UX Design",
-			body:"I was hired by an entrepeneur to take a software prototype and produce a fully designed and tested user interface.",
-			tags:["ux","psd","ai"],
-			hero:"../img/portfolio/hero_mi.jpg",
-		},
-		{
 			name:"nimble",
 			title:"Front-End Development",
+			subtitle:"Programming Responsive Web Apps",
 			body:"I code web sites and apps using HTML5, CSS3, and JS. Depending on the project, I've used CSS Preprocessors, HTML Video/Audio, and JS MVCs.",
 			tags:["html","css","js"],
 			hero:"../img/portfolio/hero_nimble.jpg",
 		},
 		{
+			name:"mi",
+			title:"UI/UX Design",
+			subtitle:"Interaction and Visual Design",
+			body:"I was hired by an entrepeneur to take a software prototype and produce a fully designed and tested user interface.",
+			tags:["ux","psd","ai"],
+			hero:"../img/portfolio/hero_mi.jpg",
+		},
+		{
 			name:"blues",
 			title:"Branding & Tech Consulting",
+			subtitle:"Web Design and Development",
 			body:"I consulted for a small business by rebranding their online presence. I redesigned their website, implemented a CMS, and expanded their brand across pertinent social media.",
 			tags:["psd","wp","webcode"],
 			hero:"../img/portfolio/hero_blues.jpg",
